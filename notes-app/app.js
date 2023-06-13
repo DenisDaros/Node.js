9493a027fccb97253aa0945edd702501e827df11
denis daros <denisv.daros@gmail.com>
2023-06-13 16:38:45 -0300

    importando módulos e testando métodos

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,10 @@
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 msg = getNotes()
 console.log(msg)
+
+message = chalk.inverse.green.bold('Success')
+
+console.log(teste)
+
