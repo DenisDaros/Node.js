31e995ac6e41cd815f7e0d0262a9750e9e0d7214
denis daros <denisv.daros@gmail.com>
2023-06-17 15:47:11 -0300

    vendo a sáida após entrada do usuário

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,9 +4,11 @@ const getNotes = require('./notes.js')
 const command = process.argv[2]
 
 //terminal: node app.js add || remove
+//terminal: node app.js add --title="things to buy"
 
 if (command === 'add') {
-    console.log('adicionando notas')
+    console.log('adicionando notas!')
 } else if(command === 'remove') {
-    console.log('remove as notas')
+    console.log('removendo notas!')
 }
+console.log(process.argv)
